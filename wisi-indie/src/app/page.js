d13f303085a8941a2ce0d85321312104a8a11255
sebastianterleira <seb.terleira1204@gmail.com>
2023-05-04 20:08:37 -0500

    (Updated) HomePage

--- wisi-indie/src/app/page.js
+++ wisi-indie/src/app/page.js
@@ -2,29 +2,14 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
-const Title = styled.h1`
-	background-image: var(--textRainbow);
-	background-color: #fff;
-	-webkit-background-clip: text;
-	-webkit-text-fill-color: transparent;
-	-moz-background-clip: text;
-	-moz-text-fill-color: transparent;
-	background-clip: text;
-	text-fill-color: transparent;
-	font-family: var(--font-family-inter);
-	font-style: normal;
-	font-weight: 900;
-	font-size: 64px;
-	line-height: 77px;
-	text-align: center;
-	letter-spacing: -3px;
-`;
-
+// Styles Navbar
 const Navbar = styled.header`
 	display: flex;
 	flex-direction: row;
 	flex-wrap: wrap;
 	justify-content: space-between;
+	align-content: center;
+	align-items: center;
 `;
 
 const NavLogoContent = styled.div`
@@ -60,18 +45,25 @@ const LogoText = styled.p`
 `;
 
 const SignIn = styled.a`
-	background-image: var(--grisPrimary);
-	background-color: #fff;
-	background-repeat: repeat;
-	-webkit-background-clip: text;
-	-webkit-text-fill-color: transparent;
-	background-clip: text;
-	text-fill-color: transparent;
+	color: rgb(212 212 216/ 0.8);
 	font-style: normal;
-	font-weight: 500;
+	font-weight: 700;
 	font-size: 16px;
 	line-height: 24px;
 	text-align: center;
+	cursor: pointer;
+
+	&:hover {
+		color: #fff;
+	}
+`;
+
+// Styles POV
+const ContentPov = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 125px 210px;
 `;
 
 const ButtonOpenSource = styled.a`
@@ -80,12 +72,13 @@ const ButtonOpenSource = styled.a`
 	align-items: center;
 	padding: 10px;
 	gap: 14px;
-	width: 230px;
-	height: 35px;
-	border: 1px solid #b2b2b2;
+	width: 229px;
+	height: 34px;
 	border-radius: 15px;
 	text-decoration: none;
+	border: 1px solid #b2b2b2;
 	margin: 0 0 13px 0;
+	justify-content: center;
 
 	&:hover {
 		.transitionColorText {
@@ -111,14 +104,23 @@ const TextButtonOpenSource = styled.h2`
 	font-size: 15px;
 	line-height: 19px;
 	text-align: center;
-
 `;
-
-const ContentPov = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	margin: 125px 210px;
+const Title = styled.h1`
+	background-image: var(--textRainbow);
+	background-color: #fff;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	-moz-background-clip: text;
+	-moz-text-fill-color: transparent;
+	background-clip: text;
+	text-fill-color: transparent;
+	font-family: var(--font-family-inter);
+	font-style: normal;
+	font-weight: 900;
+	font-size: 64px;
+	line-height: 77px;
+	text-align: center;
+	letter-spacing: -3px;
 `;
 
 const Subtitle = styled.h2`
@@ -152,17 +154,16 @@ const ButtonTryOut = styled.a`
 		#e4e4e7 53.83%,
 		rgba(255, 255, 255, 0) 180.85%
 	);
-	
+
 	border-radius: 5px;
 	text-decoration: none;
 	margin: 29px 0 0 0;
-	transition: background, filter .2s ease-in-out;
-
+	transition: background, filter 0.2s ease-in-out;
 
 	&:hover {
 		background: #e4e4e7;
 		filter: drop-shadow(-4px -4px 10px rgba(255, 255, 255, 0.1))
-		drop-shadow(4px 4px 10px rgba(255, 255, 255, 0.1));
+			drop-shadow(4px 4px 10px rgba(255, 255, 255, 0.1));
 		.right-arrow {
 			transform: translate(3px);
 		}
@@ -200,23 +201,32 @@ export default function App() {
 					<LogoText>WiseIndie</LogoText>
 				</NavLogoContent>
 				<NavButtonContent>
-					<SignIn>Sign in</SignIn>
+					<SignIn
+						target='_blank'
+						href='https://github.com/sebastianterleira/WisiIndie'
+						className='transitionSingIn'
+					>
+						Sign in
+					</SignIn>
 				</NavButtonContent>
 			</Navbar>
 			<ContentPov>
-			<ButtonOpenSource
-						target='_blank'
-						href='https://github.com/sebastianterleira/WisiIndie'
-					>
-						<TextButtonOpenSource className="transitionColorText">WiseIndie is Open Source</TextButtonOpenSource>
-						<Image
-							src='./images/ArrowRightIcon.svg'
-							alt='Logo de WiseIndie 💡'
-							width={12}
-							height={16}
-							className='right-arrow'
-						/>
-					</ButtonOpenSource>
+				<ButtonOpenSource
+					target='_blank'
+					href='https://github.com/sebastianterleira/WisiIndie'
+					className='borderGrandient'
+				>
+					<TextButtonOpenSource className='transitionColorText'>
+						WiseIndie is Open Source
+					</TextButtonOpenSource>
+					<Image
+						src='./images/ArrowRightIcon.svg'
+						alt='Logo de WiseIndie 💡'
+						width={12}
+						height={16}
+						className='right-arrow'
+					/>
+				</ButtonOpenSource>
 				<Title>CREATIVE AND INNOVATIVE SIDE PROJECTS</Title>
 				<Subtitle>
 					Fresh ideas to develop your skills and explore new horizons!
