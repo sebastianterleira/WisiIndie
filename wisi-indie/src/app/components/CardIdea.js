8004431f994920c62379257a75d86bd308248674
sebastianterleira <seb.terleira1204@gmail.com>
2023-05-08 12:12:42 -0500

    (ADD) Component CardIdea

--- wisi-indie/src/app/components/CardIdea.js
+++ wisi-indie/src/app/components/CardIdea.js
@@ -3,19 +3,22 @@ import styled from 'styled-components';
 
 const Card = styled.div`
 	width: 980px;
-	height: 104px;
+	height: 140px;
 	border-radius: 20px;
 	padding: 3px;
+	position: relative;
+	z-index: 30;
 `;
 
 const CardContent = styled.div`
 	display: flex;
-	align-items: center;
+	align-items: flex-start;
 	justify-content: center;
-	background: #d2dbee;
+	flex-direction: column;
+	// background: #1a1c20;
 	border-radius: 15%;
-	box-shadow: inset 9.91px 9.91px 15px #bfc7d9,
-		inset -9.91px -9.91px 15px #e5efff;
+	// box-shadow: inset 9.91px 9.91px 15px #bfc7d9,
+	// 	inset -9.91px -9.91px 15px #e5efff;
 
 	// 1style
 	// background: #e0e0e0;
@@ -29,23 +32,55 @@ const CardContent = styled.div`
 	border-radius: 17px;
 	width: 100%;
 	height: 100%;
+	padding: 40px;
 `;
 
-// const Text = styled.h2`
-// 	padding: 20px;
-// 	color: #fff;
-// 	font-size: 20px;
-// `;
+const Text = styled.h2`
+	font-size: 20px;
+	color: #ffffffeb;
+	font-weight: 600;
+	font-family: 'SF Pro Text', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+		Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+		'Segoe UI Symbol';
+`;
+
+const ContentInfo = styled.div`
+	display: flex;
+	width: 100%;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 19.5px 0 19.5px;
+`;
+
+const Date = styled.p`
+	font-size: 12px;
+	color: rgb(127, 127, 127);
+	font-family: var(--font-family-inter);
+`;
+
+const Autor = styled.p`
+	font-size: 12px;
+	color: rgb(127, 127, 127);
+	font-family: var(--font-family-inter);
+`;
+
+const ContentIdea = styled.div`
+	padding: 0 20px 0 20px;
+`;
 
 const CardIdea = props => {
-	// const { title, date, autor } = props.idea;
+	const { title, date, autor } = props.idea;
 	return (
 		<>
 			<Card>
 				<CardContent>
-					{/* <Text>{title}</Text>
-            <h1>{date}</h1>
-            <h1>{autor}</h1> */}
+					<ContentInfo>
+						<Autor>{`By ${autor}`}</Autor>
+						<Date>{date}</Date>
+					</ContentInfo>
+					<ContentIdea>
+						<Text>{title}</Text>
+					</ContentIdea>
 				</CardContent>
 			</Card>
 		</>
