6855c1e1a455b7608c110bc2466ada964b5998c9
sebastianterleira <seb.terleira1204@gmail.com>
2023-05-08 10:52:03 -0500

    (Updated) fakeData

--- wisi-indie/src/app/data/fakeData.js
+++ wisi-indie/src/app/data/fakeData.js
@@ -1,25 +1,25 @@
 export const Data = [
 {
   id: 1,
-  title: "Lorem impsum dolor",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nibh laoreet, efficitur purus ut, ullamcorper risus. Fusce ut tellus placerat, congue purus eu,",
   date: "2022 May 04",
   autor: "Sebastian",
 },
 {
   id: 2,
-  title: "Lorem impsum dolor",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nibh laoreet, efficitur purus ut, ullamcorper risus. Fusce ut tellus placerat, congue purus eu,",
   date: "2022 May 04",
   autor: "Sebastian",
 },
 {
   id: 3,
-  title: "Lorem impsum dolor",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nibh laoreet, efficitur purus ut, ullamcorper risus. Fusce ut tellus placerat, congue purus eu,",
   date: "2022 May 04",
   autor: "Sebastian",
 },
 {
   id: 4,
-  title: "Lorem impsum dolor",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nibh laoreet, efficitur purus ut, ullamcorper risus. Fusce ut tellus placerat, congue purus eu,",
   date: "2022 May 04",
   autor: "Sebastian",
 },
