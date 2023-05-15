f167906d00d97e2ee1b9903c1c5fcf11ccb2f616
sebastianterleira <seb.terleira1204@gmail.com>
2023-05-15 11:30:43 -0500

    Configuring Client Supabase

--- wisi-indie/supabase/client.js
+++ wisi-indie/supabase/client.js
@@ -1,1 +1,5 @@
-import {  }
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = process.env.SUPABASE_URL;
+const anomKey = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, anomKey);
