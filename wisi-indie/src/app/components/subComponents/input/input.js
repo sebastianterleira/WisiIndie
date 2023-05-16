c1fb162d1ec01373c73beb065929db9f56a85daf
Camilo-J <josecamilo1902@outlook.com>
2023-05-16 10:24:15 -0500

    Add input component to import in forms

---
+++ wisi-indie/src/app/components/subComponents/input/input.js
@@ -0,0 +1,15 @@
+const Input = ({ type, name, placeholder, label }) => {
+	return (
+		<div className='flex flex-col gap-1'>
+			<label className='text-sm font-medium text-black/80'>{label}</label>
+			<input
+				className='py-2 px-4 outline outline-1 outline-gray-500 rounded-md focus:outline-2'
+				type={type}
+				name={name}
+				placeholder={placeholder}
+			/>
+		</div>
+	);
+};
+
+export default Input;
