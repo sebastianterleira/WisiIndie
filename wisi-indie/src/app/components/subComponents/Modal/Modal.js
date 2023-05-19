2f796b287ac2ab5eac77fe5ae75a5b86ac81580c
sebastianterleira <seb.terleira1204@gmail.com>
2023-05-18 21:23:03 -0500

    (Modal) Unauthorized

--- wisi-indie/src/app/components/subComponents/Modal/Modal.js
+++ wisi-indie/src/app/components/subComponents/Modal/Modal.js
@@ -12,7 +12,7 @@ const Overlay = styled.div`
 	left: 0;
 	background: rgba(0, 0, 0, 0.6);
 	backdrop-filter: blur(30px);
-  z-index: var(--zIndex-10);
+	z-index: var(--zIndex-10);
 `;
 
 const ContenedorModal = styled.div`
@@ -32,7 +32,12 @@ const ContenedorModal = styled.div`
 			: props.width === 'lg'
 			? '574px'
 			: '200px'};
-	background: #000000;
+	background: ${props =>
+		props.background === 'light'
+			? '#fff'
+			: props.background === 'dark'
+			? '#000'
+			: '#000'};
 	border: 1px solid rgba(55, 46, 46, 0.6);
 	border-radius: 25px;
 	position: relative;
@@ -43,8 +48,6 @@ const ContenedorModal = styled.div`
 const BotonClose = styled.div`
 	color: #fff;
 	position: absolute;
-	top: 24px;
-	right: 445px;
 	background-color: transparent;
 	border: none;
 	line-height: 20px;
@@ -58,7 +61,7 @@ const BotonClose = styled.div`
 	}
 `;
 
-const Modal = ({ children, width, height, onClose }) => {
+const Modal = ({ children, width, height, onClose, fill, background }) => {
 	const modalRef = useRef(null);
 	const bodyRef = useRef(null);
 
@@ -71,13 +74,18 @@ const Modal = ({ children, width, height, onClose }) => {
 	return (
 		<>
 			<Overlay ref={bodyRef} onClick={handleClickOutside}>
-				<ContenedorModal width={width} height={height} ref={modalRef}>
+				<ContenedorModal
+					width={width}
+					height={height}
+					background={background}
+					ref={modalRef}
+				>
 					<BotonClose onClick={onClose}>
 						<svg
 							xmlns='http://www.w3.org/2000/svg'
 							width='25'
 							height='25'
-							fill='currentColor'
+							fill={fill}
 							viewBox='0 0 16 16'
 						>
 							<path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z' />
