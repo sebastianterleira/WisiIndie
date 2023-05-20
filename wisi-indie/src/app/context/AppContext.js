738dc94cde21116f851a623cbd6d03ce8d2cb413
Camilo-J <josecamilo1902@outlook.com>
2023-05-20 11:29:12 -0500

    Add function to  insert data of user in context

--- wisi-indie/src/app/context/AppContext.js
+++ wisi-indie/src/app/context/AppContext.js
@@ -1,6 +1,7 @@
 'use client';
 import { createContext, useContext } from 'react';
 import { supabase } from '../../../supabase/client';
+import { encrypt } from '../utils/handlePassword';
 
 export const AppContext = createContext();
 export const useIdea = () => {
@@ -12,7 +13,6 @@ export const useIdea = () => {
 };
 
 export const IdeaContextProvider = ({ children }) => {
-
 	// Create Idea POST
 	const createIdea = async description => {
 		const user = (await supabase.auth.getUser()).data.user;
@@ -24,15 +24,30 @@ export const IdeaContextProvider = ({ children }) => {
 					description,
 					userId: user.id,
 				})
-				.select()
-				console.log(data)
+				.select();
+			console.log(data);
 		} catch (error) {
-			console.log(error)
+			console.log(error);
 		}
 	};
 
+	const insertUserData = async (username, email, password, result) => {
+		const newPassword = await encrypt(password);
+		const { data, error } = await supabase
+			.from('User')
+			.insert({
+				email,
+				password: newPassword,
+				username,
+			})
+			.eq('id', result.data.user.id)
+			.select();
+		console.log(data);
+		console.log(error);
+	};
+
 	return (
-		<AppContext.Provider value={{ createIdea }}>
+		<AppContext.Provider value={{ createIdea, insertUserData }}>
 			{children}
 		</AppContext.Provider>
 	);
