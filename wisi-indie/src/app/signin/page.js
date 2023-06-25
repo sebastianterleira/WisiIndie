63a774ae709c87d24386963542127a2bd42f0baa
sebastianterleira <seb.terleira1204@gmail.com>
2023-06-25 16:30:02 -0500

    Form Login Formik and Validates

--- wisi-indie/src/app/signin/page.js
+++ wisi-indie/src/app/signin/page.js
@@ -1,10 +1,12 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { supabase } from '../../../supabase/client';
 import Input from '../components/subComponents/input/input';
 import { LogoSvg } from '../components/AllSvgs';
 import styled from 'styled-components';
 import ButtonOauth from '../components/subComponents/buttonOauth/buttonOauth';
+import { Formik, Form, ErrorMessage } from 'formik';
+import { useIdea } from '../context/AppContext';
 
 const LogoText = styled.p`
 	font-family: 'Inter variable', sans-serif;
@@ -16,29 +18,12 @@ const LogoText = styled.p`
 `;
 
 const SingIn = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const { userDb, getUserDabase } = useIdea();
+	const [isError, setIsError] = useState(false);
 
-	const handleSubmit = async e => {
-		e.preventDefault();
-		const { error, data } = await supabase.auth.signInWithPassword({
-			email,
-			password,
-		});
-		console.log(data);
-		if (error) {
-			console.log(error);
-		} else {
-			// Redirigir a la página de inicio
-			window.location.href = '/';
-		}
-		// console.log('testing');
-	};
-
-	const handleEvents = e => {
-		if (e.target.name === 'email') setEmail(e.target.value);
-		if (e.target.name === 'password') setPassword(e.target.value);
-	};
+	useEffect(() => {
+		getUserDabase();
+	}, []);
 
 	return (
 		<section className='bg-black-50 h-screen flex items-center justify-center'>
@@ -62,25 +47,89 @@ const SingIn = () => {
 						<div className='bg-gray-300 h-0.1 flex-1'></div>
 					</div>
 				</div>
-				<form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-					<Input
-						label={'Email'}
-						type={'email'}
-						name={'email'}
-						placeholder={'youremail@gmail.com'}
-						methodChange={handleEvents}
-					/>
-					<Input
-						label={'Password'}
-						type={'password'}
-						name={'password'}
-						placeholder={'**********'}
-						methodChange={handleEvents}
-					/>
-					<button className='w-full px-1 py-2 rounded-md box-border text-center text-white bg-black text-xs font-semibold outline focus:outline-4 focus:outline-gray-400 '>
-						CONTINUE
-					</button>
-				</form>
+				<Formik
+					initialValues={{
+						password: '',
+						email: '',
+					}}
+					validate={async valuesInputs => {
+						const ERRORS_MESSAGES = {};
+						const EMAILS_DB_AUTH = userDb.map(data => data.email);
+						const PASS_DB_AUTH = userDb.map(data => data.password);
+
+						console.log(PASS_DB_AUTH);
+
+						// validación Email
+						if (!valuesInputs.email) {
+							ERRORS_MESSAGES.email = 'Please enter an email';
+						} else if (!EMAILS_DB_AUTH.includes(valuesInputs.email)) {
+							ERRORS_MESSAGES.email = 'Invalid credential for email';
+						}
+
+						// validación Password
+						if (!valuesInputs.password) {
+							setIsError(false)
+							ERRORS_MESSAGES.password = 'Please enter an password';
+						}
+
+						return ERRORS_MESSAGES;
+					}}
+					onSubmit={async (valuesInputs, { resetForm }) => {
+						resetForm();
+						const { error, data } = await supabase.auth.signInWithPassword({
+							email: valuesInputs.email,
+							password: valuesInputs.password,
+						});
+						console.log(data);
+						if (error) {
+							console.log(error);
+							setIsError(true);
+						} else {
+							// Redirigir a la página de inicio
+							window.location.href = '/';
+						}
+						// console.log('testing');
+					}}
+				>
+					{({ errors }) => (
+						<Form className='flex flex-col gap-4'>
+							<Input
+								id='email'
+								label={'Email'}
+								type={'email'}
+								name={'email'}
+								placeholder={'youremail@gmail.com'}
+							/>
+							<ErrorMessage
+								name='email'
+								component={() => (
+									<div className='text-red-500 text-xs'>{errors.email}</div>
+								)}
+							/>
+							<Input
+								id='password'
+								label={'Password'}
+								type={'password'}
+								name={'password'}
+								placeholder={'**********'}
+							/>
+							<ErrorMessage
+								name='password'
+								component={() => (
+									<div className='text-red-500 text-xs'>{errors.password}</div>
+								)}
+							/>
+							{isError && (
+								<div className='text-red-500 text-xs'>
+									Invalid credential for password
+								</div>
+							)}
+							<button className='w-full px-1 py-2 rounded-md box-border text-center text-white bg-black text-xs font-semibold outline focus:outline-4 focus:outline-gray-400 '>
+								CONTINUE
+							</button>
+						</Form>
+					)}
+				</Formik>
 				<p className='text-xs text-gray-500'>
 					No account?
 					<a className='text-black mx-2' href='/signup'>
