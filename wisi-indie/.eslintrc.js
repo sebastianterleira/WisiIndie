45beb19e1761331b6abaa396e2ddfab64185144f
sebastianterleira <seb.terleira1204@gmail.com>
2023-06-29 11:31:32 -0500

    Configuring Rules Slint

--- wisi-indie/.eslintrc.js
+++ wisi-indie/.eslintrc.js
@@ -1,29 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  settings: {
-    react: {
-      version:'detect'
-    }
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'standard',
-    'eslint-config-prettier',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'react/prop-types': 'off',
-  }
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'react/prop-types': 'off',
+		allowPattern: '^[a-z]+(_[a-z]+)+$',
+	},
+};
