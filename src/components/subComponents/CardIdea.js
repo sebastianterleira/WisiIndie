083e40817d83d2d7bbcb8df953ae02be6793283d
sebastianterleira <seb.terleira1204@gmail.com>
2023-07-05 11:40:12 -0500

    Formater Date

--- src/components/subComponents/CardIdea.js
+++ src/components/subComponents/CardIdea.js
@@ -3,11 +3,32 @@ import styles from '../../styles/CardIdea/card-idea.module.css';
 
 const CardIdea = props => {
 	const { description, created_at, autor } = props.idea;
+
+	const MONTHS = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+
+	const date = new Date(created_at);
+	const formatDate = `${date.getFullYear()} ${
+		MONTHS[date.getMonth()]
+	} ${date.getDate()}`;
+
 	return (
 		<div className={styles['card__container']}>
 			<div className={styles['container__info']}>
 				<p className={styles['info__autor']}>{`By ${autor}`}</p>
-				<p className={styles['info__date']}>{created_at}</p>
+				<p className={styles['info__date']}>{formatDate}</p>
 			</div>
 			<div className={styles['container__description']}>
 				<h2 className={styles['description__text']}>{description}</h2>
