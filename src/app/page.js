7f6bce9a3e180f4073178dc580eb32a6562eb248
sebastianterleira <seb.terleira1204@gmail.com>
2023-07-30 11:13:22 -0500

    Add metadata Image

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,15 @@ import NavMenu from '../components/subComponents/Navbar';
 import ListIdeas from '../components/ListIdeas';
 import Footer from '../components/Footer';
 import HomeClient from '../components/subComponents/HomeClient';
+import { metadata } from './layout';
+import { Head } from 'next/document';
 
 export default function App() {
 	return (
 		<>
+			<Head>
+				<meta property='og:image' content={metadata.image} />
+			</Head>
 			<NavMenu />
 			<main className={styles.main}>
 				<section className={styles.pov}>
