895651bcea282bac4451e688a2c568bd29a4a576
sebastianterleira <seb.terleira1204@gmail.com>
2023-07-30 10:57:31 -0500

    Add metadata Image

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
 	title: 'WisiIndie',
 	description: 'WisiIndie: Creative and innovative side projects',
+  image: "/Home.png",
 	metadataBase: new URL('https://acme.com'),
 	alternates: {
 		canonical: '/',
@@ -14,10 +15,6 @@ export const metadata = {
 			'en-US': '/en-US',
 			'de-DE': '/de-DE',
 		},
-	},
-	// esto es para la imagen preview
-	openGraph: {
-		images: '/Home.png',
 	},
 	icons: {
 		icon: '/favicon.ico',
